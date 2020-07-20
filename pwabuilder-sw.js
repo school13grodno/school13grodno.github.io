4beba43f0da53319442c35b954fcead91a095f7a
EgorKrivosheev <EgorKrivosheev@users.noreply.github.com>
2020-07-20 12:51:59 +0300

    Added appcache and fixed pwa install

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,41 +1,44 @@
 // This is the "Offline page" service worker
-
 const CACHE = "pwabuilder-page";
-
 // TODO: replace the following with the correct offline fallback page
 const offlineFallbackPage = "index.html";
 
 self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+	if (event.data && event.data.type === "SKIP_WAITING") {
+		self.skipWaiting();
+	}
 });
 
 self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-  );
+	event.waitUntil(
+		caches.open(CACHE).then((cache) => cache.addAll(
+			[
+				offlineFallbackPage, 
+				'images/*',
+				'scripts/script.js',
+				'style.css'
+			]
+		))
+	);
 });
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
+	if (event.request.mode === 'navigate') {
+		event.respondWith((async () => {
+			try {
+				const preloadResp = await event.preloadResponse;
 
-        if (preloadResp) {
-          return preloadResp;
-        }
+				if (preloadResp) {
+					return preloadResp;
+				}
 
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
+				const networkResp = await fetch(event.request);
+				return networkResp;
+			} catch (error) {
+				const cache = await caches.open(CACHE);
+				const cachedResp = await cache.match(offlineFallbackPage);
+				return cachedResp;
+			}
+		})());
+	}
 });
